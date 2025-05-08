caab85e82439d2a59381ad6b73ba84844839cba5
Magnus Pladsen <magnus.pladsen@abaris.no>
2025-05-08 09:27:38 +0200

    Added mappings for split navigation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,5 +2,9 @@ return {
   n = {
     ["<C-d>"] = { "<leader>ma", desc = "Trigger leader + m + a" },
     ["<leader>P"] = { '"_dP', desc = "Better paste" },
+    ["<C-J>"] = { "<C-W><C-J>", desc = "Move to the window below" },
+    ["<C-K>"] = { "<C-W><C-K>", desc = "Move to the window above" },
+    ["<C-L>"] = { "<C-W><C-L>", desc = "Move to the window to the right" },
+    ["<C-H>"] = { "<C-W><C-H>", desc = "Move to the window to the left" },
   },
 }
