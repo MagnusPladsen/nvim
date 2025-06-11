eacf36be2a5b37a19bfb6ce2df50798e1ff52f86
Magnus Pladsen <magnus.pladsen@abaris.no>
2025-06-11 13:20:18 +0200

    Added mappings for debugprint plugin

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,7 +1,35 @@
 return {
   "andrewferrier/debugprint.nvim",
 
-  -- opts = { … },
+  opts = {
+    keymaps = {
+      normal = {
+        plain_below = "g?p",
+        plain_above = "g?P",
+        variable_below = "g?v",
+        variable_above = "g?V",
+        variable_below_alwaysprompt = "",
+        variable_above_alwaysprompt = "",
+        surround_plain = "g?sp",
+        surround_variable = "g?sv",
+        surround_variable_alwaysprompt = "",
+        textobj_below = "g?o",
+        textobj_above = "g?O",
+        textobj_surround = "g?so",
+        toggle_comment_debug_prints = "",
+        delete_debug_prints = "",
+      },
+      insert = {
+        plain = "<C-G>p",
+        variable = "<C-G>v",
+      },
+      visual = {
+        variable_below = "g?v",
+        variable_above = "g?V",
+      },
+    },
+    -- … Other options
+  },
 
   dependencies = {
     "echasnovski/mini.nvim", -- Optional: Needed for line highlighting (full mini.nvim plugin)
